3f9f91785897c404f31da9f2287981977908235c
Bioin4matics <i.malashchuk@gmail.com>
2022-02-21 13:43:18 +0000

    created a function to find project name

--- get_DNANexus_URLs.py
+++ get_DNANexus_URLs.py
@@ -7,7 +7,7 @@ import re
 
 dxpy.set_security_context({"auth_token_type": "Bearer", "auth_token": token})
 
-
+# generate download link for DNAnexus object
 def download_url(file_ID, project_ID):
     dxfile = dxpy.DXFile(file_ID)
     download_link = dxfile.get_download_url(
@@ -17,7 +17,7 @@ def download_url(file_ID, project_ID):
     )
     return download_link[0]
 
-
+# find data based on the name of the file
 def find_data(filename):
     data = list(
         dxpy.bindings.search.find_data_objects(
@@ -26,7 +26,12 @@ def find_data(filename):
     )
     return data
 
+# find project name using unique project id
+def find_project_name(project_id):
+    project_data = dxpy.bindings.dxproject.DXProject(dxid=project_id)
+    return project_data.describe().get("name")
 
+#This function generates a dataframe containing the modified names of BAM files (in Index format) including unique object ids and project id
 def create_BAM_df(BAM_Data):
     data = []
     for object in BAM_Data:
@@ -49,7 +54,7 @@ def create_BAM_df(BAM_Data):
         data, columns=["name", "folder", "pan_num", "project_id", "bam_file_id"]
     )
 
-
+# generate a dataframe containing the names of Index files including unique object ids and project id
 def create_BAI_df(Index_Data):
     data = []
     for object in Index_Data:
@@ -61,7 +66,7 @@ def create_BAI_df(Index_Data):
         data.append(merged_data)
     return pd.DataFrame(data, columns=["name", "folder", "project_id", "index_file_id"])
 
-
+# This function produces a dataframe with the URLs for BAM and Index files and 
 def final_url_links(merged_df):
     merged_df = merged_df.sort_values("project_id")
     # create new columns with url links and project name
@@ -72,12 +77,11 @@ def final_url_links(merged_df):
     bam_id = merged_df["bam_file_id"][0]
     index_id = merged_df["index_file_id"][0]
     project_id = merged_df["project_id"][0]
-    project_data = dxpy.bindings.dxproject.DXProject(dxid=project_id)
-    merged_df["project_name"][0] = project_data.describe().get("name")
     bai_name = merged_df["name"][0]
     pattern = re.compile(r"(\S+.bam)")
     pattern_search = pattern.search(bai_name)
     bam_name = pattern_search.group()
+    merged_df["project_name"][0] = find_project_name(project_id)
     merged_df["url"][0] = download_url(bam_id, project_id) + "/" + bam_name
     merged_df["indexURL"][0] = download_url(index_id, project_id) + "/" + bai_name
     # loop to fill the rest of the rows with the required data
@@ -92,8 +96,7 @@ def final_url_links(merged_df):
         if project_id == prev_project_id:
             merged_df["project_name"][i] = merged_df["project_name"][i - 1]
         else:
-            project_data = dxpy.bindings.dxproject.DXProject(dxid=project_id)
-            merged_df["project_name"][i] = project_data.describe().get("name")
+            merged_df["project_name"][i] = find_project_name(project_id)
         merged_df["url"][i] = download_url(bam_id, project_id) + "/" + bam_name
         merged_df["indexURL"][i] = download_url(index_id, project_id) + "/" + bai_name
     return merged_df.sort_values(["name", "folder"])
@@ -107,7 +110,7 @@ all_BAM_df = create_BAM_df(all_BAM)
 all_BAI = find_data("*.bam.bai")
 all_BAI_df = create_BAI_df(all_BAI)
 
-# merged two dataframes with bam and index ids using three keys:
+# merged two dataframes by matching modified bam file name with index filen name as well as folder and project id
 merged = pd.merge(all_BAM_df, all_BAI_df, on=["name", "folder", "project_id"])
 
 # generate the url links and project name
