f10d43b0ddad224415231da697c7f56eb578f7bc
Bioin4matics <i.malashchuk@gmail.com>
2022-05-12 15:50:14 +0100

    added VCF suppport

--- get_DNANexus_URLs.py
+++ get_DNANexus_URLs.py
@@ -49,6 +49,20 @@ def create_BAM_df(BAM_Data):
         data, columns=["name","bai_name", "folder", "project_id", "bam_file_id"]
     )
 
+def create_vcf_gz(BAM_Data):
+    data = []
+    for object in BAM_Data:
+        file_name = object.get("describe").get("name")
+        folder = object.get("describe").get("folder")
+        tbi_name = file_name + ".tbi"
+        object_id = object.get("describe").get("id")
+        project_id = object.get("describe").get("project")
+        merged_data = [file_name, tbi_name, folder, project_id, object_id]
+        data.append(merged_data)
+    return pd.DataFrame(
+        data, columns=["name","tbi_name", "folder", "project_id", "file_id"]
+    )
+
 # generate a dataframe containing the names of Index files including unique object ids and project id
 def create_BAI_df(Index_Data):
     data = []
@@ -60,6 +74,28 @@ def create_BAI_df(Index_Data):
         merged_data = [file_name, folder, project_id, object_id]
         data.append(merged_data)
     return pd.DataFrame(data, columns=["bai_name", "folder", "project_id", "index_file_id"])
+
+def create_tbi_df(Index_Data):
+    data = []
+    for object in Index_Data:
+        file_name = object.get("describe").get("name")
+        folder = object.get("describe").get("folder")
+        object_id = object.get("describe").get("id")
+        project_id = object.get("describe").get("project")
+        merged_data = [file_name, folder, project_id, object_id]
+        data.append(merged_data)
+    return pd.DataFrame(data, columns=["tbi_name", "folder", "project_id", "index_file_id"])
+
+def create_df(Index_Data):
+    data = []
+    for object in Index_Data:
+        file_name = object.get("describe").get("name")
+        folder = object.get("describe").get("folder")
+        object_id = object.get("describe").get("id")
+        project_id = object.get("describe").get("project")
+        merged_data = [file_name, folder, project_id, object_id]
+        data.append(merged_data)
+    return pd.DataFrame(data, columns=["name", "folder", "project_id", "vcf_file_id"])
 
 
 # This function produces a dataframe with the URLs for BAM and BAM Index files
@@ -98,6 +134,74 @@ def final_url_links(merged_df):
     merged_df = merged_df.drop(["bai_name", "project_id", "bam_file_id", "index_file_id"], axis=1)
     return merged_df.sort_values(["name", "folder"])
 
+def final_wes_url_links(merged_df):
+    #Find project names
+    uniqueProjects = merged_df["project_id"].unique()
+    project_dict = {}
+    for project in range(0, len(uniqueProjects)):
+        proj_id = uniqueProjects[project]
+        proj_name = find_project_name(proj_id)
+        project_dict[proj_id] = { 'name' : proj_name}
+    # create new columns with url links and project name
+    merged_df["project_name"] = ""
+    merged_df["url"] = ""
+    merged_df["indexURL"] = ""
+    # show progress of the loop
+    toolbar_width = len(merged_df)
+    sys.stdout.write("[%s]" % (" " * toolbar_width))
+    sys.stdout.flush()
+    sys.stdout.write("\b" * (toolbar_width+1)) # return to start of line, after '['
+    sys.stdout.flush()
+    # Generate URL links for each BAM and BAM index file in the df
+    for i in range(0, len(merged_df)):
+        tbi_name = merged_df["tbi_name"][i]
+        name = merged_df["name"][i]
+        project_id = merged_df["project_id"][i]
+        bam_id = merged_df["file_id"][i]
+        index_id = merged_df["index_file_id"][i]
+        merged_df["project_name"][i] = project_dict[project_id]['name']
+        merged_df["url"][i] = download_url(bam_id, project_id) + "/" + name
+        merged_df["indexURL"][i] = download_url(index_id, project_id) + "/" + tbi_name
+        #Show progress bar
+        sys.stdout.write("{} ".format(i+1))
+        sys.stdout.flush()
+    sys.stdout.write("]\n")
+    merged_df = merged_df.drop(["tbi_name", "project_id", "file_id", "index_file_id"], axis=1)
+    return merged_df.sort_values(["name", "folder"])
+
+def vcf_url_links(merged_df):
+    #Find project names
+    uniqueProjects = merged_df["project_id"].unique()
+    project_dict = {}
+    for project in range(0, len(uniqueProjects)):
+        proj_id = uniqueProjects[project]
+        proj_name = find_project_name(proj_id)
+        project_dict[proj_id] = { 'name' : proj_name}
+    # create new columns with url links and project name
+    merged_df["project_name"] = ""
+    merged_df["url"] = ""
+    # show progress of the loop
+    toolbar_width = len(merged_df)
+    sys.stdout.write("[%s]" % (" " * toolbar_width))
+    sys.stdout.flush()
+    sys.stdout.write("\b" * (toolbar_width+1)) # return to start of line, after '['
+    sys.stdout.flush()
+    # Generate URL links for each BAM and BAM index file in the df
+    for i in range(0, len(merged_df)):
+        name = merged_df["name"][i]
+        project_id = merged_df["project_id"][i]
+        vcf_id = merged_df["vcf_file_id"][i]
+        merged_df["project_name"][i] = project_dict[project_id]['name']
+        merged_df["url"][i] = download_url(vcf_id, project_id) + "/" + name
+        #Show progress bar
+        sys.stdout.write("{} ".format(i+1))
+        sys.stdout.flush()
+    sys.stdout.write("]\n")
+    merged_df = merged_df.drop(["project_id", "vcf_file_id"], axis=1)
+    return merged_df.sort_values(["name", "folder"])
+
+
+
 
 if __name__=="__main__":
     
@@ -107,8 +211,7 @@ if __name__=="__main__":
     print("Searching for BAM files...")
     all_BAM = find_data("*.bam", length)
     all_BAM_df = create_BAM_df(all_BAM)
-    
-   
+
     # Retrieve information for index files
     print("Searching for BAM Index files...")
     all_BAI = find_data("*.bam.bai", length)
@@ -116,13 +219,77 @@ if __name__=="__main__":
     
     # merged two dataframes by matching modified bam file name with index filen name as well as folder and project id
     print("Merging BAM and BAM Index files...")
-    merged = pd.merge(all_BAM_df, all_BAI_df, on=["bai_name", "folder", "project_id"])
-    
-    if len(merged) > 0:
-        # generate the url links and project name
-        print("Generating URL links for {} BAM and BAM Index files:".format(len(merged)))
-        url_links = final_url_links(merged)
+    merged_BAM = pd.merge(all_BAM_df, all_BAI_df, on=["bai_name", "folder", "project_id"])
+    print("Generating URL links for {} BAM and BAM Index files:".format(len(merged_BAM)))
+    BAM_url_links = final_url_links(merged_BAM)
+
+    print("Searching for TSO500 VCF files files...")
+    all_TSO = find_data("*MergedSmallVariants.genome.vcf", length)
+    all_TSO_df = create_df(all_TSO)
+    print("Generating URL links for {} TSO VCF files:".format(len(all_TSO_df)))
+    TSO_links = vcf_url_links(all_TSO_df)
+
+    print("Searching for ONC VCF files files...")
+    primer_clipped = find_data("*primerclipped.vardict.vcf", length)
+    primer_clipped_df = create_df(primer_clipped)
+    print("Generating URL links for {} ONC VCF files:".format(len(primer_clipped_df)))
+    primer_clipped_links = vcf_url_links(primer_clipped_df)
+   
+    print("Searching for SNP VCF files files...")
+    all_snp = find_data("*.sites_present_reheader_filtered_normalised.vcf", length)
+    all_snp_df = create_df(all_snp)
+    print("Generating URL links for {} ONC VCF files:".format(len(all_snp_df)))
+    snp_links = vcf_url_links(all_snp_df)
+
+    # Retrieve infomration for BAM files
+    print("Searching for WES vcf files...")
+    all_wes = find_data("*markdup_Haplotyper.vcf.gz", length)
+    all_wes_df = create_vcf_gz(all_wes)
+
+    # Retrieve information for index files
+    print("Searching for WES VCF Index files...")
+    all_wes_tbi = find_data("*Haplotyper.vcf.gz.tbi", length)
+    all_wes_tbi_df = create_tbi_df(all_wes_tbi)
+    # merging vcf and index dataframes
+    merged_wes = pd.merge(all_wes_df, all_wes_tbi_df, on=["tbi_name", "folder", "project_id"])
+
+    print("Generating URL links for {} ONC VCF files:".format(len(merged_wes)))
+    wes_url_links = final_wes_url_links(merged_wes)
+
+    url_list = []
+    if len(TSO_links) > 0:
+        url_list.append(TSO_links)
+    if len(primer_clipped_links) > 0:
+        url_list.append(primer_clipped_links)
+    if len(snp_links) > 0:
+        url_list.append(snp_links)
+    if len(BAM_url_links) > 0:
+        url_list.append(BAM_url_links)
+    if len(wes_url_links) > 0:
+        url_list.append(wes_url_links)
+
+    url_links = pd.concat(url_list, ignore_index=True)
+  
+    if url_links.empty:
+        print("No identified information was found in the time frame specified: {}".format(length))
+    else:
         url_links.to_csv(sys.argv[2], index=False, sep=",")
         print("csv file with URL links created successfully")
-    else:
-        print("No matching BAM and BAM Index files were found within the time frame specified: {}".format(length))
+
+
+'''
+Search patterns for VCF files
+-TSO
+    #MergedSmallVariants.genome.vcf
+
+-ONC
+#primerclipped.vardict.vcf
+#primerclipped.varscan.bedfiltered.vcf
+
+-WES
+#Haplotyper.vcf.gz 
+#Haplotyper.vcf.gz.tbi
+
+-SNP:
+    #.sites_present_reheader_filtered_normalised.vcf
+'''
